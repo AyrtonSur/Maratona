23847de44c36d4d75d4a98d0921903e669be0d07
AyrtonSur <ayrtonsurica@gmail.com>
2025-10-03 23:08:08 -0300

    feat: problema quase finalizado bovine genomics

--- Bovine-Genomics-2.cpp
+++ Bovine-Genomics-2.cpp
@@ -6,8 +6,8 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  // freopen("cownomics.in", "r", stdin);
-  // freopen("cownomics.out", "w", stdout);
+  freopen("cownomics.in", "r", stdin);
+  freopen("cownomics.out", "w", stdout);
 
   int n, m;
 
@@ -18,41 +18,45 @@ int main() {
   string initial_vec_chars_spotty[n];
   string initial_vec_chars_plain[n];
 
-  for (int i = 0; i < n; i++) {
-    cin >> initial_vec_chars_spotty[i];
-  }
-
-  for (int i = 0; i < n; i++) {
-    cin >> initial_vec_chars_plain[i];
-  }
-
   int total_m = (m * (m - 1) * (m - 2)) / 6;
 
-  set<char> vec_chars_spotty[total_m];
-  set<char> vec_chars_plain[total_m];
+  vector<unordered_set<string>> vec_chars_spotty(total_m);
+  vector<unordered_set<string>> vec_chars_plain(total_m);
 
-  int idx = 0;
-  for (int i = 0; i < m - 2; i++) {
-    for (int j = i + 1; j < m - 1; j++) {
-      for (int k = j + 1; k < m; k++) {
-        for (int w = 0; w < n; w++) {
-          vec_chars_spotty[idx].emplace(initial_vec_chars_spotty[w][w] + initial_vec_chars_spotty[w][w] + initial_vec_chars_spotty[k][w]);
+  for (int w = 0; w < n; w++) {
+    cin >> initial_vec_chars_spotty[w];
+
+    int idx = 0;
+
+    for (int i = 0; i < m - 2; i++) {
+      for (int j = i + 1; j < m - 1; j++) {
+        for (int k = j + 1; k < m; k++) {
+          string s(1, initial_vec_chars_spotty[w][i]); 
+          s += initial_vec_chars_spotty[w][j];
+          s += initial_vec_chars_spotty[w][k];
+          vec_chars_spotty[idx].emplace(s);
+
+          idx++;
         }
-
-        idx++;
       }
     }
   }
 
-  idx = 0;
-  for (int i = 0; i < m - 2; i++) {
-    for (int j = i + 1; j < m - 1; j++) {
-      for (int k = j + 1; k < m; k++) {
-        vec_chars_plain[idx].insert(initial_vec_chars_plain[i].begin(), initial_vec_chars_plain[i].end());
-        vec_chars_plain[idx].insert(initial_vec_chars_plain[j].begin(), initial_vec_chars_plain[j].end());
-        vec_chars_plain[idx].insert(initial_vec_chars_plain[k].begin(), initial_vec_chars_plain[k].end());
+  for (int w = 0; w < n; w++) {
+    cin >> initial_vec_chars_plain[w];
 
-        idx++;
+    int idx = 0;
+
+    for (int i = 0; i < m - 2; i++) {
+      for (int j = i + 1; j < m - 1; j++) {
+        for (int k = j + 1; k < m; k++) {
+          string s(1, initial_vec_chars_plain[w][i]); 
+          s += initial_vec_chars_plain[w][j];
+          s += initial_vec_chars_plain[w][k];
+          vec_chars_plain[idx].emplace(s);
+
+          idx++;
+        }
       }
     }
   }
@@ -69,7 +73,7 @@ int main() {
     if (vec_chars_plain[i].size() == (size1 + size2)) res++;
   }
 
-  cout << res;
+  cout << res << endl;
 
   return 0;
 }
